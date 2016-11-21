8b343f19d205a7fbe798f216c77a3e34d09ff202
Fredrik <fredrikbeckman97@gmail.com>
2016-11-21 19:14:47 +0100

    Dag 1 Part 2
    ss

---
+++ Strings/src/Application.java
@@ -0,0 +1,16 @@
+
+public class Application {
+	public static void main(String[] args) {
+		String text = "Hello!";
+		String blank = "  ";
+		String name = "Fredrik";
+		String greeting = text + blank + name;
+		
+		int myInt = 8;
+		double myDouble = 7.29;
+		System.out.println(greeting);
+		System.out.println("My integer is;" + myInt);
+		System.out.println("My double is;" + myDouble);
+	}
+
+}
