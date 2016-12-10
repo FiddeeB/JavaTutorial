fdafeec397ae03bd197bc3122b3b889ac4cc99a8
Fredrik Beckman <fredrikbeckman97@gmail.com>
2016-12-10 10:53:32 +0100

    Mergesort
    sss

---
+++ Mergesort/src/com/java2novice/sorting/MyMergeSort.java
@@ -0,0 +1,67 @@
+/*
+ * Created by fredr on 2016-12-09.
+ */
+package com.java2novice.sorting;
+
+class MyMergeSort {
+
+    private int[] array;
+    private int[] tempMergArr;
+
+    public static void main(String a[]){
+
+        int[] inputArr = {45,23,11,89,77,98,4,28,65,43, 55, 22, 33, 77, 77, 88, 22 , 3,5, 778, 74, 42, 147, 752, 4540};
+        MyMergeSort mms = new MyMergeSort();
+        mms.sort(inputArr);
+        for(int i:inputArr){
+            System.out.print(i);
+            System.out.print(" ");
+        }
+    }
+
+    private void sort(int inputArr[]) {
+        this.array = inputArr;
+        int length = inputArr.length;
+        this.tempMergArr = new int[length];
+        doMergeSort(0, length - 1);
+    }
+
+    private void doMergeSort(int lowerIndex, int higherIndex) {
+
+        if (lowerIndex < higherIndex) {
+            int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
+            // Below step sorts the left side of the array
+            doMergeSort(lowerIndex, middle);
+            // Below step sorts the right side of the array
+            doMergeSort(middle + 1, higherIndex);
+            // Now merge both sides
+            mergeParts(lowerIndex, middle, higherIndex);
+        }
+    }
+
+    private void mergeParts(int lowerIndex, int middle, int higherIndex) {
+
+        for (int i = lowerIndex; i <= higherIndex; i++) {
+            tempMergArr[i] = array[i];
+        }
+        int i = lowerIndex;
+        int j = middle + 1;
+        int k = lowerIndex;
+        while (i <= middle && j <= higherIndex) {
+            if (tempMergArr[i] <= tempMergArr[j]) {
+                array[k] = tempMergArr[i];
+                i++;
+            } else {
+                array[k] = tempMergArr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i <= middle) {
+            array[k] = tempMergArr[i];
+            k++;
+            i++;
+        }
+
+    }
+}
